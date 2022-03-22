3980dba68858bb2e66d9b4d26ea9aa724d476765
CuetoA <cuetorra@DESKTOP-TIS34QV.localdomain>
2022-03-22 15:18:32 -0600

    Modified targets

--- P1-Marte-CodeBlocks/Pre-mkmarte/Para_workspace_MaRTE_OS/MaRTEOS_script.sh
+++ P1-Marte-CodeBlocks/Pre-mkmarte/Para_workspace_MaRTE_OS/MaRTEOS_script.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Way of use of this script:
 # source MaRTE_script.sh 
-GNAT=/home/usuario/myapps/gnat/bin 
-export  PATH="$GNAT:/home/usuario/marte_2.0_22Feb2017/utils:$PATH"
-export  PERL5LIB=/home/usuario/marte_2.0_22Feb2017
-echo $PATH | cut -d ':' -f -5 | tr ':' '\n'
+GNAT=/home/cuetorra/myapps/gnat/bin 
+export  PATH="$GNAT:/home/cuetorra/marte_2.0_22Feb2017/utils:$PATH"
+export  PERL5LIB=/home/cuetorra/marte_2.0_22Feb2017
+# echo $PATH | cut -d ':' -f -5 | tr ':' '\n'
